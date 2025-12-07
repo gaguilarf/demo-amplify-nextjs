f103a808eb8f516d88097e7a2cdda316dc3470b3
GUSTAVO FERNANDO AGUILAR FLORES <gaguilarf@unsa.edu.pe>
2025-12-06 20:50:25 -0500

    feat(main): add Amplify backend data model for Todos and a corresponding Next.js frontend UI for CRUD operations.

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -40,7 +40,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -50,9 +50,6 @@ export default function App() {
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
       </div>
       <button onClick={signOut}>Sign out</button>
     </main>

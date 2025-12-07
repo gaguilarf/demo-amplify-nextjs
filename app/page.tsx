f3b5ae83de50b627e53b8e5313e7439588827530
GUSTAVO FERNANDO AGUILAR FLORES <gaguilarf@unsa.edu.pe>
2025-12-06 19:26:40 -0500

    feat(main): Initialize Next.js application with AWS Amplify authentication and data operations.

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -24,6 +26,11 @@ export default function App() {
   useEffect(() => {
     listTodos();
   }, []);
+
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   function createTodo() {
     client.models.Todo.create({
@@ -37,7 +44,7 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -47,6 +54,7 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
